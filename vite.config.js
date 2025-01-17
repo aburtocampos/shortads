f5fce0e3edca1aa4302fd85f730d50bb33ca4ea5
Enrique <eabucam@gmail.com>
2025-01-17 17:16:29 -0600

    redirect

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.' // Copia en la raíz de `dist`
+        }
+      ]
+    })
+  ]
+});
